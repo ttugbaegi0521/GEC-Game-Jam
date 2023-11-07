d19a58a502f22207fc9947c6bb8628a6b4a03da5
ttugbaegi0521 <ttugbaegi0521@gmail.com>
2023-11-07 20:51:02 +0900

    start

---
+++ gecgamejam/src/Components/Menubar/Menubar.js
@@ -0,0 +1,13 @@
+import React form 'react';
+import './Menubar.css'
+
+const Menubar = () => {
+  
+  return (
+    <div className={"menubar"}>
+      <h1>GEC Game Jam</h1>
+      <h3>St. Johnsbury Academy</h3>
+      <h3>Login</h3>
+    </div>
+  )
+}
